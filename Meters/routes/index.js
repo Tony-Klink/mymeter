5664682304f4bce840096b71cd7632c5d90e4273
Tony Klink <ziranitarium@gmail.com>
2016-11-18 14:04:42 +0900

    aaaaa

--- Meters/routes/index.js
+++ Meters/routes/index.js
@@ -1,9 +1,22 @@
 ﻿var express = require('express');
 var router = express.Router();
 
+
+
 /* GET home page. */
 router.get('/', function (req, res) {
     res.render('index', { title: 'Express', name: 'Yan' });
 });
+
+router.get('/index', function (req, res) {
+    res.render('index', { title: 'Express', name: 'Yan' });
+});
+
+router.post('/meter', function (req, res) {
+    console.log(req.body);
+    res.redirect('/meter/' + req.body.meter_id + '/' + req.body.start_date + '/' + req.body.end_date);
+    
+});
+
 
 module.exports = router;
